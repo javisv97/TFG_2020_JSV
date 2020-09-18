ae79b052238566a04b31c4b4c37b94816b9a43d5
Javier Sánchez <32193946+javisv97@users.noreply.github.com>
2020-09-18 20:07:46 +0100

    First commit
    Commit of .py files

---
+++ create_data_lists.py
@@ -0,0 +1,6 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+    create_data_lists(voc07_path='./data/NewData/train',
+                      voc12_path='./data/NewData/test',
+                      output_folder='./')
